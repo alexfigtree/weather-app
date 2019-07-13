8860217ee9e2f5b8e6e475e090b55126ca3e2925
Alex <alexandra.higuera@gmail.com>
2019-07-13 12:38:40 -0400

    Implement semantic ui css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 
 import React, { Component } from 'react';
+import { Button, Dropdown, Input } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+
 import logo from './logo.svg';
 import './App.css';
 
 
-
+const searchOptions = [
+  {
+    key: 'City',
+    text: 'City',
+    value: 'City'
+  },
+  {
+    key: 'Zip',
+    text: 'Zip',
+    value: 'Zip'
+  },
+]
 
 class App extends Component {
   constructor(props) {
@@ -48,7 +62,7 @@ class App extends Component {
     const APIKEY =  '18aaef05aea27a3ddbb3d40975b82b7a';
     console.log('get getWeatherLatLong called iwth lat', lat);
     //fetch("api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long)
-    fetch("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&APPID=18aaef05aea27a3ddbb3d40975b82b7a")
+    fetch("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&APPID=18aaef05aea27a3ddbb3d40975b82b7a&units=imperial")
       .then(res => res.json())
       .then(
         (json) => {
@@ -98,7 +112,21 @@ class App extends Component {
     //console.log('lateuts', latitude);
     
     return (
-      <div>
+      <div className="App">
+        <div className="title">Weather app</div>
+        <h4>Search by</h4>
+        <Dropdown
+          placeholder='City or zip'
+          fluid
+          selection
+          options={searchOptions}
+        />
+        <br />
+        <h4>Search for your location</h4>
+        <Input focus placeholder='Enter city or zip' />
+        <br />
+        <Button>Lets go!</Button>
+        <br />
         <input type="text" value={latitude} />
         <input type="text" value={longitude} />
         <div>temp: {temp}</div>
